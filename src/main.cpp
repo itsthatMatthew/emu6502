36739391428f94fa9b2bcacc01d3e33356f5399e
itsthatMatthew <csizmadiamate22@gmail.com>
2026-03-08 01:37:42 +0100

    chores: add explainer to the hand-coded asm

--- src/main.cpp
+++ src/main.cpp
@@ -54,12 +54,13 @@ int main() {
   cpu.registers = {0, 0, 0, 0, 0, 0};
 
   printf("Setting up memory...\n");
-  memory[0x00] = 0xA9;
-  memory[0x01] = 0x10;
-  memory[0x02] = 0x69;
-  memory[0x03] = 0x01;
-  memory[0x04] = 0x85;
-  memory[0x05] = 0x0F;
+  memory[0x00] = 0xA9; // LDA #$10 ; Load 16 to the accumulator
+  memory[0x01] = 0x10; // 
+  memory[0x02] = 0x69; // ADC #$01 ; Add 1 to the accumulator
+  memory[0x03] = 0x01; //
+  memory[0x04] = 0x85; // STA $07  ; Store the accumulator's value at memory
+  memory[0x05] = 0x07; //          ; location 0x07 (observe on 4.c. when ran)
+  memory[0x06] = 0x00; // BRK ; causes a memory exception for now, hence stops
   
   printf("Setup finished, starting execution...\n\n");
   printstuff();
